622d5f14ed8c57489875f23b6a8119b6636cd3b7
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-20 20:48:15 -0700

    updates deploy.js and readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This project creates and deploys and ERC20 token.  A React frontend can interact with the token contract.  
 
-The purpose is to demonstrate how to bind a frontend to a contract on an Ethereum network.  This was done as part of the scale Ethereum hackathon.
+The purpose is to demonstrate how to bind a frontend to a contract on an Ethereum network with Ethers.js and MetaMask.  This was done as part of the scale Ethereum hackathon.
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,21 @@
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  //This script needs to be updated so that the deployer is a real account so that it can be put on a testnet
 
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const MyToken = await ethers.getContractFactory('MyToken');
-  const exp = ethers.BigNumber.from('10').pow(18);
-  const initialValue = ethers.BigNumber.from('100').mul(exp);
-  const myToken = await MyToken.deploy(initialValue);
-
-  console.log("Token address:", myToken.address);
+  // const [deployer] = await ethers.getSigners();
+  //
+  // console.log(
+  //   "Deploying contracts with the account:",
+  //   deployer.address
+  // );
+  //
+  // console.log("Account balance:", (await deployer.getBalance()).toString());
+  //
+  // const MyToken = await ethers.getContractFactory('MyToken');
+  // const exp = ethers.BigNumber.from('10').pow(18);
+  // const initialValue = ethers.BigNumber.from('100').mul(exp);
+  // const myToken = await MyToken.deploy(initialValue);
+  //
+  // console.log("Token address:", myToken.address);
 }
 
 main()
