b29733a4eb3e3d7c8ff4d4b9921400bf59c9df3b
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-21 00:06:43 -0700

    finishes useEffect()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,63 +6,60 @@ import MyToken from './contracts/contracts/MyToken.sol/MyToken.json';
 import contractAddress from './contracts/contracts/contract-address.json';
 
 function App() {
-  console.log('contractAddress: ', contractAddress);
+  let [isConnected, setIsConnected] = useState();
+  let [isLoading, setIsLoading] = useState();
+  let [currentMetaMaskAccount, setCurrentMetaMaskAccount] = useState(null);
 
-  // let [isConnected, setIsConnected] = useState();
-  // let [isLoading, setIsLoading] = useState();
-  //
-  // useEffect(() => {
-  //   const init = async () => {
-  //     //detect whether the browser is connected to a provider
-  //     const provider = await detectEthereumProvider();
-  //     if (provider) {
-  //       startApp(provider);
-  //     } else {
-  //       alert('Please install MetaMask!');
-  //       setIsConnected(false);
-  //       setIsLoading(false);
-  //       return;
-  //     };
-  //
-  //     async function startApp(provider) {
-  //       //The provider detected by detectEthereumProvider() is the same as window.ethereum
-  //       if (provider !== window.ethereum) {
-  //         alert('Do you have multiple wallets installed?');
-  //         setIsConnected(false);
-  //         setIsLoading(false);
-  //         return;
-  //       };
-  //
-  //       //ADD CHAIN ID CODE FROM METAMASK DOCS
-  //
-  //       const ethersProvider = new ethers.providers.Web3Provider(provider);
-  //
-  //
-  //       const myToken = new ethers.Contract();
-  //
-  //       //create ricer instance to interact with deployed Ricer contract
-  //       const networkId = await web3.eth.net.getId();
-  //       const deploymentNetwork = Ricer.networks[networkId];
-  //       if (deploymentNetwork !== undefined) {
-  //         const deployedRicer = new web3.eth.Contract(
-  //           Ricer.abi,
-  //           deploymentNetwork.address
-  //         );
-  //         setRicer(deployedRicer);
-  //       };
-  //
-  //       let accounts = await provider.request({ method: 'eth_accounts' });
-  //       if (accounts.length > 0) {
-  //         setIsConnected(true);
-  //         setCurrentMetaMaskAccount(accounts[0]);
-  //       };
-  //     };
-  //   };
-  //   init();
-  // }, []);
+  useEffect(() => {
+    const init = async () => {
+      //detect whether the browser is connected to a provider
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        startApp(provider);
+      } else {
+        alert('Please install MetaMask!');
+        setIsConnected(false);
+        setIsLoading(false);
+        return;
+      };
+
+      async function startApp(provider) {
+        //The provider detected by detectEthereumProvider() is the same as window.ethereum
+        if (provider !== window.ethereum) {
+          alert('Do you have multiple wallets installed?');
+          setIsConnected(false);
+          setIsLoading(false);
+          return;
+        };
+
+        //ADD CHAIN ID CODE FROM METAMASK DOCS
+        //POLLING?
+        //ERROR HANDLING
+
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
+        //Create a reference to the deployed MyToken contract
+        const myToken = new ethers.Contract(
+          contractAddress.MyToken,
+          MyToken.abi,
+          ethersProvider
+        );
+        //Create a reference in state to the current MetaMask account
+        let accounts = await provider.request({ method: 'eth_accounts' });
+        if (accounts.length > 0) {
+          setIsConnected(true);
+          setCurrentMetaMaskAccount(accounts[0]);
+        };
+      };
+    };
+    init();
+  }, []);
 
   return (
     <div className="app-margin">
+      <div>
+        The current user address: {currentMetaMaskAccount}
+      </div>
+      <br></br>
       La la la...
     </div>
   );
